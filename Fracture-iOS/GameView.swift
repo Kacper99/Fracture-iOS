c61c0946d8a94e0a4a020714ee20b013c30a3ba9
Kacper Martela <kacper99@hotmail.co.uk>
2018-07-16 23:17:10 +0100

    Added a basic UI

---
+++ Fracture-iOS/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  Fracture-iOS
+//
+//  Created by Kacper Martela on 16/07/2018.
+//  Copyright © 2018 Kacper Martela. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameView: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
